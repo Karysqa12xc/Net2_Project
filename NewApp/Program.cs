a42732de08d87c3100caf6546797f951afa890a6
Karysqa <85331340+Karysqa12xc@users.noreply.github.com>
2023-09-23 23:02:02 +0700

    Method Has Type Return

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,16 +3,8 @@ using NewApp.Models;
 
 // TODO: Khoi tao doi tuong
 Person ps1 = new Person();
-Person ps2 = new Person();
-SinhVien std1 = new SinhVien();
-//* gan gia tri cho thuoc tinh doi tuong ps1
-ps1.FullName = "Nguyen Van A";
-ps1.Address = "Ha Noi";
-ps1.Age = 18;
 //* Khai bao 2 bien va gan gia tri
-string str = "Nguyen Van B";
+string str = "Nguyen Van A";
 int a = 22;
-//* Goi phuong thuc hien thi thong tin
-ps1.Display();
-ps2.Display();
-std1.Display(str, a);
+Console.WriteLine($"{str} sinh nam {ps1.GetYearOfBirth(a)}");
+
