a3f91973c9ae7eb6e034c7ed7b0bea27defb1d97
Karysqa <85331340+Karysqa12xc@users.noreply.github.com>
2023-09-30 16:37:24 +0700

    Hoan Thanh Bai Tap Array List

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,25 +1,9 @@
-﻿using NewApp.Models;
+﻿using System.Collections;
+using NewApp.Models;
 //* Xây dựng phương thức thêm, xoá, sửa với kiểu collection
 //* Array list
-Console.WriteLine($"Nhap so luong person: ");
-int n = int.Parse(Console.ReadLine() ?? "0");
 
-Person[] People = new Person[n];
-
-for (int i = 0; i < People.Length; i++)
-{
-    Console.WriteLine($"Them thong tin person {i}: ");
-    Person ps = new Person();
-    ps.EnterData();
-    People[i] = ps;
-}
-
-Console.WriteLine($"Nhap id cua person muon sua doi: ");
-int id = int.Parse(Console.ReadLine() ?? "0");
-People[id].ModifierInfoPerson();
-
-foreach (var Person in People)
-{
-    Console.WriteLine($"In Ra thong tin person  : ");
-    Person.Display();
-}
+ArrayList p = new ArrayList();
+Person.CreatePersonByArrayList(p);
+Person.ModifierArrayList(p, "PS01");
+Person.PrintAllInfoArrayList(p);
