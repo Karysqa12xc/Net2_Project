dcef4825b5bd4edbfea50244d6d31819affbc8b5
Karysqa <85331340+Karysqa12xc@users.noreply.github.com>
2023-10-07 09:58:22 +0700

    remove new app

--- NewApp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using NewApp.Models;
-//* Xây dựng phương thức thêm, xoá, sửa với kiểu collection
-//* Array list
-
-ArrayList p = new ArrayList();
-Person.CreatePersonByArrayList(p);
-Person.ModifierArrayList(p, "PS01");
-Person.PrintAllInfoArrayList(p);
